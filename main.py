ed9d5cb22dbe892e30e11e99d207a8a50bdd368c
Orazio Cappadonna <oraziocappadonna1999@gmail.com>
2021-03-11 23:39:24 -0400

    Primera version del bot, funcionando

---
+++ main.py
@@ -0,0 +1,112 @@
+#Discord library, is asynchronous
+import discord
+import os
+import requests
+import json
+import random
+from replit import db
+from keep_alive import keep_alive
+
+
+#This is the connection to discord
+client = discord.Client()
+
+sad_words = ["sad","unhappy","depressed","depressing","miserable"]
+
+starter_encouragements = [
+	"Cheer up!",
+	"Hang in there",
+	"You are a great person!"
+]
+
+if "responding" not in db.keys():
+	db["responding"] = True
+
+if db["encouragements"] not in db.keys():
+	db["encouragements"] = starter_encouragements
+
+
+def get_quote():
+	response = requests.get("https://zenquotes.io/api/random")
+	json_data = json.loads(response.text)
+	quote = json_data[0]['q'] + " -" + json_data[0]['a']
+	return quote
+
+
+def update_encouragements(encouraging_message):
+	if "encouragements" in db.keys():
+		encouragements = db["encouragements"]
+		encouragements.append(encouraging_message)
+		db["encouragements"] = encouragements
+	else:
+		db["encouragements"] = encouraging_message
+
+
+def delete_encouragement(index):
+	encouragements = db["encouragements"]
+	if len(encouragements) > index:
+		del encouragements[index]
+		db["encouragements"] = encouragements
+
+
+#This is how i register an event, using decorators
+@client.event
+async def on_ready():
+	#This is taking the client, the zero is replaced with the client, getting the username
+	print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+	if message.author == client.user:
+		return
+
+	msg = message.content
+
+	#Doing this, this is like a command the bot will read and do something
+	if message.content.startswith('$hello'):
+		#So, everytime someone writes "$hello", the bot will answer "Hello!"
+		await message.channel.send('Hello!')
+
+	if message.content.startswith('$inspire'):
+		quote = get_quote()
+		await message.channel.send(quote)
+
+	if db["responding"]:	
+		options = db["encouragements"]
+
+		if any(word in msg for word in sad_words):
+			await message.channel.send(random.choice(options))
+
+	if msg.startswith("$new"):
+		encouraging_message = msg.split("$new ", 1)[1]
+		update_encouragements(encouraging_message)
+		await message.channel.send("New encouraging message added")
+
+	if msg.startswith("$del"):
+		encouragements = []
+		if "encouragements" in db.keys():
+			index = int(msg.split("$del", 1)[1])
+			delete_encouragement(index)
+			encouragements = db["encouragements"]
+		await message.channel.send(encouragements)
+
+	if msg.startswith("$list"):
+		encouragements = []
+		if "encouragements" in db.keys():
+			encouragements = db["encouragements"]
+			await message.channel.send(encouragements)
+
+	if msg.startswith("$responding"):
+		value = msg.split("$responding ", 1)[1]
+
+		if value.lower() == "true":
+			db["responding"] = True
+			await message.channel.send("Responding is on.")
+		else:
+			db["responding"] = False
+			await message.channel.send("Responding is off.")
+	
+
+keep_alive()
+client.run(os.getenv('TOKEN'))
